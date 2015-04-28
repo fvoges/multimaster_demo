856eed028f408907bf4cdaf9d907ec9549932857
Federico Voges <federico@puppetlabs.com>
2015-04-28 16:57:17 +0200

    Fixed path

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 rm -rf /etc/puppetlabs/puppet/environments/production/
 puppet module install zack-r10k --modulepath=/etc/puppetlabs/puppet/modules/
-git clone https://github.com/fvoges/multimaster_demo.git puppet/environments/production
+git clone https://github.com/fvoges/multimaster_demo.git /etc/puppetlabs/puppet/environments/production
 
 puppet apply -t setup_r10k.pp
